accfd1a2af1d6ff54d46aa48e060ae7d9f3a4c25
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2024-08-30 16:56:54 +0300

    collect api metrics

--- src/main/java/com/techsol/metrics/TrafficMonitor.java
+++ src/main/java/com/techsol/metrics/TrafficMonitor.java
@@ -7,20 +7,27 @@ import io.prometheus.client.exporter.HTTPServer;
 
 public class TrafficMonitor {
     static final Counter pageVisits = Counter.build().name("page_visits_total").help("Total page visits.")
-            .labelNames("methodName", "endpoint", "ipAddress").register();
+            .labelNames("httpMethod", "endpoint", "ipAddress").register();
 
     static final Counter errors = Counter.build().name("errors_total").help("Total number of errors.")
-            .labelNames("methodName", "errorType", "ipAddress", "endpoint").register();
+            .labelNames("httpMethod", "errorType", "ipAddress", "endpoint").register();
+
+            static final Counter apiRequests = Counter.build().name("api_requests").help("Total number of requests.")
+            .labelNames("methodName", "httpMethod", "endpoint", "responseCode").register();
 
     public static void init() throws IOException {
         HTTPServer server = new HTTPServer(8085);
     }
 
-    public static void logPageVisit(String methodName, String endpoint, String ipAddress) {
-        pageVisits.labels(methodName, endpoint, ipAddress).inc();
+    public static void logPageVisit(String httpMethod, String endpoint, String ipAddress) {
+        pageVisits.labels(httpMethod, endpoint, ipAddress).inc();
     }
 
-    public static void logErrors(String methodName, String errorType, String ipAddress, String endpoint) {
-        errors.labels(methodName, errorType, ipAddress, endpoint).inc();
+    public static void logErrors(String httpMethod, String errorType, String ipAddress, String endpoint) {
+        errors.labels(httpMethod, errorType, ipAddress, endpoint).inc();
+    }
+
+    public static void logAPIRequests(String methodName, String httpMethod, String endpoint, String responseCode) {
+        apiRequests.labels(methodName, httpMethod, endpoint, responseCode).inc();
     }
 }
--- src/main/java/com/techsol/resources/Configuration.java
+++ src/main/java/com/techsol/resources/Configuration.java
@@ -1,23 +1,29 @@
 package com.techsol.resources;
 
-import com.techsol.services.SiteStarterService;
-import jakarta.inject.Inject;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.List;
+
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
 import org.json.JSONObject;
 
+import com.techsol.metrics.TrafficMonitor;
+import com.techsol.services.SiteStarterService;
+
+import jakarta.inject.Inject;
 import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.container.ResourceInfo;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
-import java.io.InputStream;
-import java.util.List;
 
 @Path("/configure")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 * 10, maxRequestSize = 1024 * 1024 * 100)
@@ -25,22 +31,48 @@ public class Configuration {
     @Inject
     private SiteStarterService siteStarterService;
 
+    @Context
+    private ResourceInfo resourceInfo;
+
+    @Context
+    private HttpServletRequest servletRequest;
+
     @GET
     @Path("/applicationPort")
     @Produces(MediaType.APPLICATION_JSON)
     public Response applicationPort(@QueryParam("port") String port) {
         JSONObject responseObject = new JSONObject();
 
+        Method method = resourceInfo.getResourceMethod();
+        String methodName = null;
+        String endpoint = null;
+        String httpMethod = null;
+
+        if (method != null) {
+            methodName = method.getName();
+        }
+
+        if (servletRequest != null) {
+            endpoint = servletRequest.getRequestURI();
+            httpMethod = servletRequest.getMethod();
+        }
+
         if (port == null || port.isEmpty()) {
             responseObject.put("statusMessage", "Port not provided");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
             return Response.status(400).entity(responseObject.toString()).build();
         }
 
         if (siteStarterService.isPortAvailable(Integer.parseInt(port))) {
             responseObject.put("statusMessage", "Port is available");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "200");
             return Response.ok(responseObject.toString()).build();
         } else {
             responseObject.put("statusMessage", "Port " + port + " is not available");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
             return Response.status(Response.Status.BAD_REQUEST).entity(responseObject.toString()).build();
         }
     }
@@ -56,9 +88,25 @@ public class Configuration {
         String siteName = null;
         String port = null;
 
+        Method method = resourceInfo.getResourceMethod();
+        String methodName = null;
+        String endpoint = null;
+        String httpMethod = null;
+
+        if (method != null) {
+            methodName = method.getName();
+        }
+
+        if (servletRequest != null) {
+            endpoint = servletRequest.getRequestURI();
+            httpMethod = servletRequest.getMethod();
+        }
+
         for (Attachment attachment : attachments) {
             if (attachment == null) {
                 responseObject.put("statusMessage", "Please check the request payload");
+
+                TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
                 return Response.status(Response.Status.BAD_REQUEST).entity(responseObject.toString()).build();
             } else {
                 String paramName = attachment.getContentDisposition().getParameters().get("name");
@@ -72,20 +120,30 @@ public class Configuration {
 
         if (fileInputStream == null && siteName == null && port == null) {
             responseObject.put("statusMessage", "Site files, site name, and port not provided");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
             return Response.status(Response.Status.BAD_REQUEST).entity(responseObject.toString()).build();
         } else if (fileInputStream == null) {
             responseObject.put("statusMessage", "Site files not provided");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
             return Response.status(Response.Status.BAD_REQUEST).entity(responseObject.toString()).build();
         } else if (siteName == null) {
             responseObject.put("statusMessage", "Site name not provided");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
             return Response.status(Response.Status.BAD_REQUEST).entity(responseObject.toString()).build();
         } else if (port == null) {
             responseObject.put("statusMessage", "Port not provided");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "400");
             return Response.status(Response.Status.BAD_REQUEST).entity(responseObject.toString()).build();
         }
 
         if (!siteStarterService.uploadFiles(fileInputStream, siteName)) {
             responseObject.put("statusMessage", "Failed to upload site files");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "500");
             return Response.serverError().entity(responseObject.toString()).build();
         }
 
@@ -93,10 +151,14 @@ public class Configuration {
 
         if (startServerResponse.isEmpty()) {
             responseObject.put("statusMessage", "Failed to start server");
+
+            TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "500");
             return Response.serverError().entity(responseObject.toString()).build();
         }
 
         responseObject.put("statusMessage", "Started server on port " + port + " successfully");
+
+        TrafficMonitor.logAPIRequests(methodName, httpMethod, endpoint, "200");
         return Response.ok(responseObject.toString()).build();
     }
 }
