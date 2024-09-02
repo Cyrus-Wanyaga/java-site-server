f4d0f2ce7bf4c2a92a78921d489d951509e65491
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2024-09-02 16:47:40 +0300

    add bootstrap headers to website requests only

--- src/main/java/com/techsol/web/filters/CSPFilter.java
+++ src/main/java/com/techsol/web/filters/CSPFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class CSPFilter implements Filter {
@@ -14,9 +15,15 @@ public class CSPFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Content-Security-Policy",
-                "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com; script-src 'self' https://stackpath.bootstrapcdn.com https://code.jquery.com;");
+        HttpServletRequest req = (HttpServletRequest) request;
+        String requestUrl = req.getRequestURI();
+
+        if (requestUrl != null && !requestUrl.contains("/api/")) {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.setHeader("Content-Security-Policy",
+                    "default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com; script-src 'self' https://stackpath.bootstrapcdn.com https://code.jquery.com;");
+        }
+
         chain.doFilter(request, response);
     }
 
